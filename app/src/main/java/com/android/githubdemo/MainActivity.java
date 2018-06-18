1ab15698c752d2e4281e0eeb4f2bc7e0d90781c7
YinAnt <514239201@qq.com>
2018-06-18 17:34:05 +0800

    测试2

--- app/src/main/java/com/android/githubdemo/MainActivity.java
+++ app/src/main/java/com/android/githubdemo/MainActivity.java
@@ -8,9 +8,16 @@ public class MainActivity extends AppCompatActivity {
 //    Can't finish GitHub sharing process
 //    Successfully created project 'GitHubDemo' on GitHub, but initial push failed: unable to access 'https://github.com/YinAnt/GitHubDemo.git/': Could not resolve host: github.com
 
+
+//    Push failed
+//    Failed with error: fatal: HttpRequestException encountered. fatal: unable to access 'https://github.com/YinAnt/GitHubDemo.git/': The requested URL returned error: 403
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 111111111111111111111111111
     }
 }
